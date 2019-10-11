d9c3c1697755e800460030dc1d71818d1d5e0813
Srdan Vorkapic <srvorkap@e1z3r6p22.42.us.org>
2019-10-10 22:21:15 -0700

    Libft

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,7 +6,7 @@
 /*   By: srvorkap <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/04 00:00:02 by srvorkap          #+#    #+#             */
-/*   Updated: 2019/10/09 23:04:22 by srvorkap         ###   ########.fr       */
+/*   Updated: 2019/10/10 21:00:59 by srvorkap         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,57 +33,58 @@ static int	nb_of_strings(char const *str, char c)
 
 static char	**memgiver(char const *str, char c)
 {
-    char    **res;
-    int     letters;
-    int     i;
-    int     j;
-    if (!(res = (char **)malloc(sizeof(char*) * (nb_of_strings(str, c) + 1))))
-        return (NULL);
-    i = 0;
-    j = 0;
-    while (str[i])
-    {
-        letters = 0;
-        while (str[i] == c && str[i])
-            i++;
-        while (str[i] != c && str[i] != '\0')
-        {
-            letters++;
-            i++;
-        }
-        if (letters > 0)
-            if ((res[j++] = (char *)malloc(sizeof(char) * letters + 1)) == NULL)
-                return (NULL);
-    }
-    res[j] = 0;
-    return (res);
+	char	**res;
+	int		letters;
+	int		i;
+	int		j;
+
+	if (!(res = (char **)malloc(sizeof(char*) * (nb_of_strings(str, c) + 1))))
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (str[i])
+	{
+		letters = 0;
+		while (str[i] != '\0' && str[i] == c)
+			i++;
+		while (str[i] != '\0' && str[i] != c)
+		{
+			letters++;
+			i++;
+		}
+		if (letters > 0)
+			if (!(res[j++] = (char *)malloc(sizeof(char) * letters + 1)))
+				return (NULL);
+	}
+	res[j] = 0;
+	return (res);
 }
 
 char		**ft_strsplit(char const *s, char c)
 {
-	char    **tab;
-    int     len;
-    int     i;
-    int     j;
-    int     pos;
+	char	**tab;
+	int		len;
+	int		i;
+	int		j;
+	int		pos;
 
-    if (s == NULL)
-        return (NULL);
-    tab = memgiver(s, c);
-    len = nb_of_strings(s, c);
-    if (tab == NULL)
-        return (NULL);
-    i = 0;
-    pos = 0;
-    while (pos < len)
-    {
-        while (s[i] != '\0' && s[i] == c)
-            i++;
-        j = 0;
-        while (s[i] != '\0' && s[i] != c)
-            tab[pos][j++] = s[i++];
-        tab[pos][j] = '\0';
-        pos++;
-    }
-    return (tab);
+	if (s == NULL)
+		return (NULL);
+	tab = memgiver(s, c);
+	len = nb_of_strings(s, c);
+	if (tab == NULL)
+		return (NULL);
+	i = 0;
+	pos = 0;
+	while (pos < len)
+	{
+		while (s[i] != '\0' && s[i] == c)
+			i++;
+		j = 0;
+		while (s[i] != '\0' && s[i] != c)
+			tab[pos][j++] = s[i++];
+		tab[pos][j] = '\0';
+		pos++;
+	}
+	return (tab);
 }
